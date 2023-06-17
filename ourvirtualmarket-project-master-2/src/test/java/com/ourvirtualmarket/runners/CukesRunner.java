353cf1da097a7e4e1701417f1313d64ded433502
Bulent <bulent5556@gmail.com>
2023-06-17 11:19:16 +0300

    searc function stepdefs added

--- ourvirtualmarket-project-master-2/src/test/java/com/ourvirtualmarket/runners/CukesRunner.java
+++ ourvirtualmarket-project-master-2/src/test/java/com/ourvirtualmarket/runners/CukesRunner.java
@@ -13,8 +13,8 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/ourvirtualmarket/steps_defs",
-        dryRun = false,
-        tags = "@ovm"
+        dryRun = true,
+        tags = "@wip"
 )
 public class CukesRunner {
 
