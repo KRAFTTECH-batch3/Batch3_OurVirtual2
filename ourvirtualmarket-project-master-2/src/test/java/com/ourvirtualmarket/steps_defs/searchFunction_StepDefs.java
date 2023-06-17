81944016de77e500de822fd3d2c794f52e33db2c
Bulent <bulent5556@gmail.com>
2023-06-17 11:21:16 +0300

    searc function stepdefs added

--- ourvirtualmarket-project-master-2/src/test/java/com/ourvirtualmarket/steps_defs/searchFunction_StepDefs.java
+++ ourvirtualmarket-project-master-2/src/test/java/com/ourvirtualmarket/steps_defs/searchFunction_StepDefs.java
@@ -6,27 +6,22 @@ import io.cucumber.java.en.Then;
 public class searchFunction_StepDefs {
     @Given("The search box should appear on the main page.")
     public void the_search_box_should_appear_on_the_main_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @Given("the {string} text should appear in the search bar.")
     public void the_text_should_appear_in_the_search_bar(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @Given("Any order can be entered in the search bar")
     public void any_order_can_be_entered_in_the_search_bar() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @Given("the search button should be clicked")
     public void the_search_button_should_be_clicked() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @Then("the user should be directed to the page where the products related to the given order are found.")
     public void the_user_should_be_directed_to_the_page_where_the_products_related_to_the_given_order_are_found() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 }
