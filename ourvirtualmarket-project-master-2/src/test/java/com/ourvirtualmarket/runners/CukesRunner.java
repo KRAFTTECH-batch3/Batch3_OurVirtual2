730e7a1df0be69675a785a812b0187a7a18c959c
Busra <119009610+bsr-hyt@users.noreply.github.com>
2023-06-21 12:16:03 +0300

    Update CukesRunner.java

--- ourvirtualmarket-project-master-2/src/test/java/com/ourvirtualmarket/runners/CukesRunner.java
+++ ourvirtualmarket-project-master-2/src/test/java/com/ourvirtualmarket/runners/CukesRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/ourvirtualmarket/steps_defs",
         dryRun = false,
-        tags = "@B3VIR2-217"
+        tags = "@addToCart"
 )
 
 public class CukesRunner {
