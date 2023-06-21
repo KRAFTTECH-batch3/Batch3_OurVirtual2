26a248ce61530c69fddc36321ec8926c2b7318f5
cengizyavas <cngzyvs3706@gmail.com>
2023-06-21 11:04:43 +0300

    The latest version of alternative add to cart function codes (reviewed)

--- ourvirtualmarket-project-master-2/src/test/java/com/ourvirtualmarket/runners/CukesRunner.java
+++ ourvirtualmarket-project-master-2/src/test/java/com/ourvirtualmarket/runners/CukesRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/ourvirtualmarket/steps_defs",
         dryRun = false,
-        tags = "@us_20"
+        tags = "@ovm"
 )
 
 public class CukesRunner {
