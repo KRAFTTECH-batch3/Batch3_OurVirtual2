a550008039fea35b81f9ea4c06351e5989c85958
feridulucinar <fulbaibu@gmail.com>
2023-06-22 00:26:45 +0300

    Quick view feature and stepdefs added

---
+++ ourvirtualmarket-project-master-2/src/test/java/com/ourvirtualmarket/steps_defs/Quickview_StepDefs.java
@@ -0,0 +1,44 @@
+package com.ourvirtualmarket.steps_defs;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class Quickview_StepDefs {
+
+    @Given("the user hovers the mouse over any product")
+    public void the_user_hovers_the_mouse_over_any_product() {
+    }
+    @Then("the {string} button appears automatically")
+    public void the_button_appears_automatically(String string) {
+
+    }
+    @Given("the user clicks on {string}")
+    public void the_user_clicks_on(String string) {
+
+    }
+    @Then("a pop-up appears that has details of the product")
+    public void a_pop_up_appears_that_has_details_of_the_product() {
+
+    }
+
+    @Given("the user clicks on the {string} button")
+    public void the_user_clicks_on_the_button(String string) {
+
+    }
+    @Then("the item is added successfully and the total cost is shown to the customer correctly.")
+    public void the_item_is_added_successfully_and_the_total_cost_is_shown_to_the_customer_correctly() {
+
+    }
+
+    @Given("the user goes to the top of the home page")
+    public void the_user_goes_to_the_top_of_the_home_page() {
+
+    }
+    @Then("the total cost is changed correctly")
+    public void the_total_cost_is_changed_correctly() {
+
+    }
+
+
+
+ }
--- ourvirtualmarket-project-master-2/src/test/resources/features/quickView.feature
+++ ourvirtualmarket-project-master-2/src/test/resources/features/quickView.feature
@@ -1,1 +1,27 @@
-Feature: QuickView
+@B3VIR2-225
+Feature: US 16: Quick View
+
+
+  @QuickViewTC1 @TEST_B3VIR2-192
+  Scenario: TC_01 Quick View _ hovers the mouse over any product, the "Quick View" button appears automatically.
+    Given the user hovers the mouse over any product
+    Then the "Quick View" button appears automatically
+
+
+  @QuickViewTC2 @TEST_B3VIR2-193
+  Scenario: TC_02_the user clicks on "Quick view", pop up appears
+    Given the user clicks on "Quick view"
+    Then a pop-up appears that has details of the product
+
+
+  @QuickViewTC3 @TEST_B3VIR2-194
+  Scenario: TC_03_Clicks on the "Add To Cart" button
+
+    Given the user clicks on the "Add To Cart" button
+    Then the item is added successfully and the total cost is shown to the customer correctly.
+
+  @QuickViewTC4 @TEST_B3VIR2-195
+  Scenario: TC_04_ total cost is changed correctly at the right top of the home page
+
+  Given the user goes to the top of the home page
+  Then the total cost is changed correctly
