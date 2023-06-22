67065caf5abf13a8ead33e38248882cfe783426c
emrekcmn <emre.kcmn15@gmail.com>
2023-06-22 16:11:25 +0300

    updated logoutFunctionality.feature tags

--- ourvirtualmarket-project-master-2/src/test/java/com/ourvirtualmarket/steps_defs/Logout_StepDefs.java
+++ ourvirtualmarket-project-master-2/src/test/java/com/ourvirtualmarket/steps_defs/Logout_StepDefs.java
@@ -25,35 +25,29 @@ public class Logout_StepDefs {
     SoftAssertions softAssertions = new SoftAssertions();
     @Given("The user click the Logout button at the top right corner")
     public void click_the_logout_button_at_the_top_right_corner() {
-       // dashboardPage=new DashboardPage();
         dashboardPage.logout();
     }
     @When("Verify that title is {string}")
     public void verify_that_title_is(String expectedTitle) {
         String actualTitle = Driver.get().getTitle();
-        //Assert.assertEquals(expectedTitle,actualTitle);
     softAssertions.assertThat(actualTitle).isEqualTo(expectedTitle);
     }
     @When("Verify that the logout helper texts")
     public void verify_that_the_logout_helper_texts() {
-       // logoutPage =new AccountLogoutPage();
         String expectedHelperText1 = "You have been logged off your account. It is now safe to leave the computer.";
         String expectedHelperText2="Your shopping cart has been saved, the items inside it will be restored whenever you log back into your account.";
         String actualHelperText1 = logoutPage.helperText1.getText();
         String actualHelperText2= logoutPage.helperText2.getText();
         softAssertions.assertThat(actualHelperText1).isEqualTo(expectedHelperText1);
         softAssertions.assertThat(actualHelperText2).isEqualTo(expectedHelperText2);
-//        Assert.assertEquals(expectedHelperText1,actualHelperText1);
-//        Assert.assertEquals(expectedHelperText2,actualHelperText2);
+
 
 
     }
     @When("Verify that the Continue button is visible")
     public void verify_that_the_button_is_visible() {
-       // logoutPage =new AccountLogoutPage();
         softAssertions.assertThat(logoutPage.continueButton.isDisplayed());
         softAssertions.assertAll();
-       // Assert.assertTrue(logoutPage.continueButton.isDisplayed());
         BrowserUtils.waitFor(1);
 
     }
@@ -64,53 +58,38 @@ public class Logout_StepDefs {
     }
     @Then("Verify that Logout button is Not visible")
     public void verify_that_and_button_is_visible_and_button_is_not_visible() {
-      //  dashboardPage= new DashboardPage();
+
         JavascriptExecutor jse =(JavascriptExecutor) Driver.get();
         jse.executeScript("window.scrollBy(0,-700)");
         BrowserUtils.waitFor(1);
-//       softAssertions.assertThat(dashboardPage.loginButton.isDisplayed());
-//        softAssertions.assertThat(dashboardPage.registerButton);
-//        //Assert.assertFalse(dashboardPage.logoutButton.isDisplayed());
-//        //Assert.assertFalse(dashboardPage.registerButton.isDisplayed());
 
         softAssertions.assertThat(dashboardPage.logoutButton.isDisplayed()).isFalse();
         softAssertions.assertAll();
-
-
-       // BrowserUtils.verifyElementNotDisplayed(By.xpath("//a[text()=' Logout ']"));
-
-
 
     }
     @Then("Verify that title is not {string}")
     public void verify_that_title_is_not(String title) {
         String actualTitle = Driver.get().getTitle();
-       // Assert.assertNotEquals(title, actualTitle);
         softAssertions.assertThat(actualTitle).isNotEqualTo(title);
-
     }
 
 
     @Given("The user click the Logout button at the down right of the page")
     public void clickTheLogoutButtonAtTheDownRightOfThePage() {
-       // myAccountPage =new MyAccountPage();
         JavascriptExecutor jse =(JavascriptExecutor) Driver.get();
         jse.executeScript("window.scrollBy(0,500)");
         BrowserUtils.waitFor(1);
 
-        //  BrowserUtils.clickWithJS(myAccountPage.alternativeLogoutButton);
         myAccountPage.alternativeLogoutButton.click();
     }
 
     @Given("The user go to the {string} module")
     public void theUserGoToTheModule(String tab) {
-       // dashboardPage=new DashboardPage();
         dashboardPage.navigateToModule(tab);
     }
 
     @And("The user select the {string} product")
     public void theUserSelectTheProduct(String productName) {
-        //productsPage = new ProductsPage();
         productsPage.navigateToProduct(productName);
 
     }
@@ -121,27 +100,27 @@ public class Logout_StepDefs {
                 "//p[@class='text-shopping-cart']/../../../following-sibling::*"));
         String actualText= element.getText();
         String expectedText = "Your shopping cart is empty!";
-        //Assert.assertEquals(expectedText,actualText);
+
         softAssertions.assertThat(actualText).isEqualTo(expectedText);
     }
     @Then("Verify that Shopping cart page is NOT visible")
     public void verifyThatShoppingCartPageIsNOTVisible() {
         WebElement element = Driver.get().findElement(By.cssSelector("[class='table-responsive']"));
-        //Assert.assertFalse(element.isDisplayed());
+
         softAssertions.assertThat(element.isDisplayed()).isFalse();
 
     }
 
     @Given("Click the {string} button")
     public void clickTheButton(String tab) {
-      // myAccountPage =new MyAccountPage();
+
        myAccountPage.navigateToLeftTabs(tab);
     }
 
     @Given("Verify that First Name {string}, Last Name {string}, email {string} and Telephone {string}")
     public void verifyThatFirstNameLastNameEmailAndTelephone(String expectedFirstName, String expectedLastName,
                                                              String expectedEmail, String expectedTelephone) {
-        //myAccountPage =new MyAccountPage();
+
         String actualFirstName = myAccountPage.firstNameInputBox.getAttribute("value");
         String actualLastName = myAccountPage.lastNameInputBox.getAttribute("value");
         String actualEmail = myAccountPage.emailInputBox.getAttribute("value");
@@ -154,8 +133,6 @@ public class Logout_StepDefs {
 
     @Then("Verify that My Account Information is NOT visible")
     public void verifyThatMyAccountInformationIsNOTVisible() {
-       // myAccountPage =new MyAccountPage();
-   //     Assert.assertFalse(myAccountPage.personalDetailForm.isDisplayed());
         softAssertions.assertThat(myAccountPage.personalDetailForm.isDisplayed()).isFalse();
     }
 }
